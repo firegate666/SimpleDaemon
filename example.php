1c6a11e45ed4b334f5fff0351e5f410442f6f3dc
Marco Behnke <marco@behnke.biz>
2015-10-06 22:04:22 +0200

    add file logger

--- example.php
+++ example.php
@@ -5,12 +5,14 @@ use firegate666\Logger\ConsoleLogger;
 use firegate666\Daemon\Configuration;
 use firegate666\Daemon\Daemon;
 use firegate666\Daemon\Example\HandlerFactory;
+use firegate666\Logger\FileLogger;
 
 declare(ticks = 1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$logger = new ConsoleLogger();
+//$logger = new ConsoleLogger();
+$logger = new FileLogger(__DIR__ . '/logs/info.log');
 
 $handler_factory = new HandlerFactory();
 $handler_factory->setLogger($logger);
