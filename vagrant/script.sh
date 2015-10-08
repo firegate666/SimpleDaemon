8799deb80e4e1c2c78de69724bc2739ca33e1f73
Marco Behnke <marco@behnke.biz>
2015-10-08 14:42:56 +0200

    install htop

--- vagrant/script.sh
+++ vagrant/script.sh
@@ -2,5 +2,6 @@
 
 /usr/bin/sudo /usr/bin/apt-get -q -y update
 /usr/bin/sudo /usr/bin/apt-get -q -y install php5-cli
+/usr/bin/sudo /usr/bin/apt-get -q -y htop
 
 /usr/bin/curl -sS https://getcomposer.org/installer | /usr/bin/sudo /usr/bin/php -- --install-dir=/usr/bin/ --filename=composer
